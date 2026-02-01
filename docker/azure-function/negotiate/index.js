e9479a17f35abd6fd15eac3f7a22beafa0a27502
Ninotarabini <vicentetarabini@gmail.com>
2026-02-01 13:57:57 +0100

    feat: migrate SignalR negotiate to Azure Function serverless

---
+++ docker/azure-function/negotiate/index.js
@@ -0,0 +1,12 @@
+module.exports = async function (context, req, connectionInfo) {
+    context.res = {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        },
+        body: connectionInfo
+    };
+};
