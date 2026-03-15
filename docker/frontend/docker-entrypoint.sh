28f9f7aee93c2ab365f4041a371231bb03156e05
Ninotarabini <vicentetarabini@gmail.com>
2026-03-15 13:52:31 +0100

    FEAT(infra): implement automated cache busting and nginx cache control

--- docker/frontend/docker-entrypoint.sh
+++ docker/frontend/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 envsubst '${BACKEND_URL} ${ENVIRONMENT} ${APP_VERSION}' < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
+envsubst '${APP_VERSION}' < /usr/share/nginx/html/index.template.html > /usr/share/nginx/html/index.html
 exec nginx -g "daemon off;"
